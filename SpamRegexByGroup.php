06babd192fea7484a2cc7686772f99e64d7c9e87
Philipp Gruber <philipp@luigi.(none)>
2012-09-24 20:26:54 +1000

    basic frame

--- SpamRegexByGroup.php
+++ SpamRegexByGroup.php
@@ -5,5 +5,19 @@
     Allow different Spam Blacklists per Group
  **********************************************************/
 
+$wgExtensionCredits['antispam'][] = array(
+    'path' => __FILE__,
+    'name' => 'SpamRegexByGroup',
+    'author' => 'Philipp Gruber', 
+    'url' => '', 
+    'description' => 'Define different spamRegex values per group',
+    'version'  => 0.1,
+);
+
+$wgHooks['ArticleSave'][] = 'efSpamRegexByGroup';
+
+function efSpamRegexByGroup(&$article, &$user, &$text, &$summary, $minor, $watch, $sectionanchor, &$flags) {
+    return true;
+}
 
 ?>
